3ae49a7dd9b8b9973cbbcf237426b892693b012b
Timur <timurgol007@gmail.com>
2024-11-30 01:01:05 +0300

    change parse options

--- src/main.py
+++ src/main.py
@@ -9,11 +9,11 @@ from picture_entropy import VideoEntropy
 
 repo_path = Path(__file__).parent.parent
 
-def generate_entropy_png(picture: str, output: str, diff: bool):
+def generate_entropy_png(picture: str, output: str, diff: str):
     path_to_picture = Path(picture)
     path_to_output = Path(output)
 
-def generate_wav_plot(audio: str, output: str, diff: bool):
+def generate_wav_plot(audio: str, output: str, diff: str):
     path_to_audio = Path(audio)
     path_to_output = Path(output)
 
@@ -21,6 +21,7 @@ def generate_encoded_video(video: str, secret: str, output: str):
     path_to_video = Path(video)
     path_to_secret = Path(secret)
     path_to_output = Path(output)
+    with 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,12 +40,12 @@ if __name__ == "__main__":
     entropy = subparsers.add_parser("entropy_png")
     entropy.add_argument("--picture", type=str, required=True, help="path to picture png")
     entropy.add_argument("--output", required=True, help="path to picture entropy plot")
-    entropy.add_argument("--diff", default=False, type=bool, action="store_true", help="plot diff between encoded picture and raw")
+    entropy.add_argument("--diff", default="", type=str, help="path to encoded picture to see diff")
 
     plot = subparsers.add_parser("wav_plot")
     plot.add_argument("--audio", required=True, help="path to audio wav")
     plot.add_argument("--output", required=True, help="path to audio plot")
-    plot.add_argument("--diff", help="plot diff between encoded audio and raw")
+    plot.add_argument("--diff", default="", type=str, help="path to encoded audio to see diff")
 
     args = parser.parse_args()
     print(args)
